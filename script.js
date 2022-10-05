c0a5afed847eb6a8323cc51e6a19d8f7a87be072
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2022-10-05 08:28:13 -0300

    Atualização Projeto tela de login

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ function logar() {
     if(senha == novasenha && login == novologin){
         alert('OK')
     }else{
-        alert('Algo esta errado!')
+        document.querySelector('#capture').addEventListener('click', function (e) {
+            alert('Algo está errado!')
+            var canvas = document.querySelector("#canvas");  
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+            var context = canvas.getContext('2d');
+            context.drawImage(video, 0, 0)
+        })
     }
+
 }
+navigator.mediaDevices.getUserMedia({video: true})
+.then(function (mediaStream) {
+    const video = document.querySelector('#video');
+    video.srcObject = mediaStream;
+    video.play();
+})
